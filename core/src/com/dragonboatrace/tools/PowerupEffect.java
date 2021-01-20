772cfae76f7db850e0475d54c73870a0c63793c1
dk949 <david2000katz@gmail.com>
2021-01-20 18:40:51 +0000

    Made the functional interface

---
+++ core/src/com/dragonboatrace/tools/PowerupEffect.java
@@ -0,0 +1,13 @@
+package com.dragonboatrace.tools;
+
+import com.dragonboatrace.entities.boats.Boat;
+
+/**
+ * Functional interface, used to name the type of the stored lambda expressions,
+ * which dictate what the powerup does.
+ * This is very similar to built in Consumer&lt;T> interface.
+ * However it avoids the need to pass the type to it. It is also more clearly named.
+ */
+public interface PowerupEffect {
+    void invoke(Boat boat);
+}
