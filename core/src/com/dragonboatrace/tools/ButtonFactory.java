b1f4a94763293dcc88cc20ff872d7373ab27cd09
dk949 <david2000katz@gmail.com>
2021-01-22 22:05:50 +0000

    Added some documentation.

--- core/src/com/dragonboatrace/tools/ButtonFactory.java
+++ core/src/com/dragonboatrace/tools/ButtonFactory.java
@@ -5,10 +5,28 @@ import com.badlogic.gdx.math.Vector2;
 import com.dragonboatrace.entities.Button;
 import com.dragonboatrace.entities.EntityType;
 
+/**
+ * Contains Static methods used to create various buttons.
+ * Each method takes just a singe texturePrefix
+ * which gets turned into full texture names by appending either '_active' or '_inactive' to the end of it.
+ */
 public class ButtonFactory {
 
+    /**
+     * Static variable used to keep track of how many times the mainMenu method is called
+     */
     private static int mainMenuButtonCount = 0;
 
+    /**
+     * Creates the buttons on the MainMenuScreen.
+     * yOffset is how far off the bottom edge of the screen the first button is.
+     * spacing is how much space there is between the bottom of one button and the bottom of another.
+     * mainMenuButtonCount increments automatically,
+     * so the buttons get spaced correctly without needing to provide exact coordinates for each.
+     *
+     * @param texturePrefix The first part of the name of the texture.
+     * @return the instance of Button.
+     */
     public static Button mainMenu(String texturePrefix) {
         float yOffset = 100f;
         float spacing = 10f + EntityType.BUTTON.getHeight();
@@ -22,6 +40,11 @@ public class ButtonFactory {
         );
     }
 
+    /**
+     * Creates the 'back 'button in the HelpScreen. Button is created at 0,0.
+     * @param texturePrefix The first part of the name of the texture.
+     * @return the instance of Button.
+     */
     public static Button help(String texturePrefix) {
         return new Button(
                 new Vector2(0, 0),
@@ -30,16 +53,24 @@ public class ButtonFactory {
         );
     }
 
+    /**
+     * Static variable used to keep track of how many times the boatSelect method is called.
+     */
     private static int boatSelectionButtonCount = 0;
 
+    /**
+     * Creates the buttons on the BoatSelectScreen.
+     * xOffset is how far off the left edge of the screen the first button is.
+     * spacing is how much space there is between the left edge of one button and the left edge of another.
+     * boatSelectionButtonCount increments just like it does in mainMenu().
+     * @param texturePrefix The first part of the name of the texture.
+     * @return the instance of Button.
+     */
     public static Button boatSelect(String texturePrefix) {
         float xOffset = (Gdx.graphics.getWidth() - EntityType.BUTTON.getWidth() * 4.0f) / 5.0f;
+        float spacing = (EntityType.BUTTON.getWidth() + xOffset);
         return new Button(
-                new Vector2(
-                        xOffset +
-                                (EntityType.BUTTON.getWidth() + xOffset) *
-                        boatSelectionButtonCount++,
-                        100),
+                new Vector2(xOffset + spacing * boatSelectionButtonCount++, 100),
                 texturePrefix + "_active.png",
                 texturePrefix + "_inactive.png"
         );
--- core/src/com/dragonboatrace/tools/VectorFactory.java
+++ core/src/com/dragonboatrace/tools/VectorFactory.java
@@ -7,19 +7,35 @@ import com.dragonboatrace.entities.EntityType;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Contains static methods to create various vectors.
+ */
 public class VectorFactory {
-    public static Vector2 randomPosition (EntityType entityType, float laneLeftBound, int laneWidth) {
+    /**
+     * Returns a random position inside of a lane.
+     * @param entityType type of an entity this is the position of.
+     * @param laneLeftBound x value of the left edge of the lane.
+     * @param laneWidth width of the lane
+     * @return Vector2 of the position
+     */
+    public static Vector2 randomPosition(EntityType entityType, float laneLeftBound, int laneWidth) {
         float middleOfLane = ((int) laneLeftBound + laneWidth) / 2.0f;
         return new Vector2(
                 ((int) laneLeftBound + laneWidth) / 2.0f +
                         ThreadLocalRandom.current().nextInt(
-                                -(int)middleOfLane + entityType.getWidth() / 2,
-                                (int)middleOfLane + entityType.getWidth() / 2
+                                -(int) middleOfLane + entityType.getWidth() / 2,
+                                (int) middleOfLane + entityType.getWidth() / 2
                         ),
                 Gdx.graphics.getHeight());
     }
 
-    public static Vector2 boatPosition(float laneLeftBound, int laneWidth){
+    /**
+     * Returns a boat's starting position in a lane
+     * @param laneLeftBound x value of the left edge of the lane.
+     * @param laneWidth width of the lane
+     * @return Vector2 of the position
+     */
+    public static Vector2 boatPosition(float laneLeftBound, int laneWidth) {
         return new Vector2(
                 laneLeftBound + (laneWidth - EntityType.BOAT.getWidth()) / 2.0f,
                 100
