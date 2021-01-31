e42087395fcf1648d61ce051286d0a8cfff066ff
dk949 <david2000katz@gmail.com>
2021-01-31 04:18:59 +0000

    Added Vector2 to the list of types that can be saved

--- core/src/com/dragonboatrace/tools/Prefs.java
+++ core/src/com/dragonboatrace/tools/Prefs.java
@@ -2,68 +2,80 @@ package com.dragonboatrace.tools;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Json;
 
 public class Prefs {
     private static final Preferences prefs = Gdx.app.getPreferences("save_data");
     private static final Json json = new Json();
-    public static class Save{
-        public static void open(){
+
+    public static class Save {
+        public static void open() {
             prefs.putInteger("save_exists", 1);
         }
 
-        public static void close(){
+        public static void close() {
             prefs.flush();
         }
 
-        public static void putInteger(String key, int i){
+        public static void putInteger(String key, int i) {
             prefs.putInteger(key, i);
         }
 
-        public static void putFloat(String key, float f){
+        public static void putFloat(String key, float f) {
             prefs.putFloat(key, f);
         }
 
-        public static void putString(String key, String s){
+        public static void putString(String key, String s) {
             prefs.putString(key, s);
         }
 
-        public static void putArray(String key, float[] f){
-            prefs.putString(key, json.toJson(f, float[].class));
+        public static void putArray(String key, Float[] f) {
+            prefs.putString(key, json.toJson(f, Float[].class));
+        }
+
+        public static void putVector2(String key, Vector2 v) {
+            prefs.putFloat(key + 'x', v.x);
+            prefs.putFloat(key + 'y', v.y);
         }
 
     }
 
     public static class Restore {
         public static void open() throws SaveDoesNotExist {
-            if(!prefs.contains("save_exists")) {
+            if (!prefs.contains("save_exists")) {
                 throw new SaveDoesNotExist("No save data found");
             }
         }
 
-        public static void close(){} // Exists only for consistency
+        public static void close() {
+        } // Exists only for consistency
 
-        public static int getInteger(String key){
+        public static int getInteger(String key) {
             return prefs.getInteger(key);
         }
 
-        public static float getFloat(String key){
+        public static float getFloat(String key) {
             return prefs.getFloat(key);
         }
 
-        public static String getString(String key){
+        public static String getString(String key) {
             return prefs.getString(key);
         }
 
-        public static float[] getArray(String key){
-            return json.fromJson(float[].class,prefs.getString(key));
+        public static Float[] getArray(String key) {
+            return json.fromJson(Float[].class, prefs.getString(key));
         }
 
+        public static Vector2 getVector2(String key) {
+            return new Vector2(prefs.getFloat(key + 'x'), prefs.getFloat(key + 'y'));
+
+        }
     }
 
 
     public static class SaveDoesNotExist extends Exception {
-        public SaveDoesNotExist (String errorMessage) {
+        public SaveDoesNotExist(String errorMessage) {
             super(errorMessage);
         }
     }
