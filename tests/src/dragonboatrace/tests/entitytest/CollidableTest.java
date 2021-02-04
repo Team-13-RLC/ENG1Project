0733ad7e4770ab6a7963c8ffcc857c60ddd545c8
Will-Dunlop <64005534+Will-Dunlop@users.noreply.github.com>
2021-02-04 10:06:03 +0000

    Added a CollidableTest file.

---
+++ tests/src/dragonboatrace/tests/entitytest/CollidableTest.java
@@ -0,0 +1,31 @@
+package dragonboatrace.tests.entitytest;
+
+import com.dragonboatrace.entities.CollidableType;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import com.badlogic.gdx.Gdx;
+import dragonboatrace.tests.GdxTestRunner;
+import com.dragonboatrace.entities.Collidable;
+
+import java.util.ArrayList;
+
+@RunWith(GdxTestRunner.class);
+public class CollidableTest {
+
+    ArrayList<Collidable> collidableArrayList;
+
+
+    @Before
+    public void setup(){
+        for (CollidableType type : CollidableType.values()){
+            Collidable
+        }
+    }
+
+    @Test
+    public void UpdateTest(){
+
+    }
+
+}
