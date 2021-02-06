6d4364856ce8d35097f4de1fd8f2c001fd7dddf5
Will-Dunlop <64005534+Will-Dunlop@users.noreply.github.com>
2021-02-06 13:24:45 +0000

    Added tests for all collidable update methods.

--- tests/src/dragonboatrace/tests/entitytest/CollidableTest.java
+++ tests/src/dragonboatrace/tests/entitytest/CollidableTest.java
@@ -36,5 +36,61 @@ public class CollidableTest {
         assertTrue("Y position should move.",collidableArrayList.get(0).getPos().y < pre_y);
         assertFalse("X position shouldn't move.",collidableArrayList.get(0).getPos().x != pre_x);
     }
+    @Test
+    public void testBranchUpdate(){
+        float pre_x = collidableArrayList.get(1).getPos().x;
+        float pre_y = collidableArrayList.get(1).getPos().y;
+        collidableArrayList.get(1).update(2,5);
+        assertTrue("Y position should move.",collidableArrayList.get(1).getPos().y < pre_y);
+        assertFalse("X position shouldn't move.",collidableArrayList.get(1).getPos().x != pre_x);
+    }
+    @Test
+    public void testLeafUpdate(){
+        float pre_x = collidableArrayList.get(2).getPos().x;
+        float pre_y = collidableArrayList.get(2).getPos().y;
+        collidableArrayList.get(2).update(2,5);
+        assertTrue("Y position should move.",collidableArrayList.get(2).getPos().y < pre_y);
+        assertFalse("X position shouldn't move.",collidableArrayList.get(2).getPos().x != pre_x);
+    }
+    @Test
+    public void testInvulnUpdate(){
+        float pre_x = collidableArrayList.get(3).getPos().x;
+        float pre_y = collidableArrayList.get(3).getPos().y;
+        collidableArrayList.get(3).update(2,5);
+        assertTrue("Y position should move.",collidableArrayList.get(3).getPos().y < pre_y);
+        assertFalse("X position shouldn't move.",collidableArrayList.get(3).getPos().x != pre_x);
+    }
+    @Test
+    public void testSpeedUpUpdate(){
+        float pre_x = collidableArrayList.get(4).getPos().x;
+        float pre_y = collidableArrayList.get(4).getPos().y;
+        collidableArrayList.get(4).update(2,5);
+        assertTrue("Y position should move.",collidableArrayList.get(4).getPos().y < pre_y);
+        assertFalse("X position shouldn't move.",collidableArrayList.get(4).getPos().x != pre_x);
+    }
+    @Test
+    public void testLessDamageUpdate(){
+        float pre_x = collidableArrayList.get(5).getPos().x;
+        float pre_y = collidableArrayList.get(5).getPos().y;
+        collidableArrayList.get(5).update(2,5);
+        assertTrue("Y position should move.",collidableArrayList.get(5).getPos().y < pre_y);
+        assertFalse("X position shouldn't move.",collidableArrayList.get(5).getPos().x != pre_x);
+    }
+    @Test
+    public void testLessTimeUpdate(){
+        float pre_x = collidableArrayList.get(6).getPos().x;
+        float pre_y = collidableArrayList.get(6).getPos().y;
+        collidableArrayList.get(6).update(2,5);
+        assertTrue("Y position should move.",collidableArrayList.get(6).getPos().y < pre_y);
+        assertFalse("X position shouldn't move.",collidableArrayList.get(6).getPos().x != pre_x);
+    }
+    @Test
+    public void testHealUpdate(){
+        float pre_x = collidableArrayList.get(7).getPos().x;
+        float pre_y = collidableArrayList.get(7).getPos().y;
+        collidableArrayList.get(7).update(2,5);
+        assertTrue("Y position should move.",collidableArrayList.get(7).getPos().y < pre_y);
+        assertFalse("X position shouldn't move.",collidableArrayList.get(7).getPos().x != pre_x);
+    }
 
 }
