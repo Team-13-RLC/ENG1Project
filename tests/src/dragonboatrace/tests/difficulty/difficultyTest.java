54228daec3e4de5eae50646db20769fcdd018591
NamLDuong <namlinhduong@gmail.com>
2021-02-06 13:28:45 +0000

    created two new folders for the tools package testing and the difficulty testing.

---
+++ tests/src/dragonboatrace/tests/difficulty/difficultyTest.java
@@ -0,0 +1,27 @@
+package dragonboatrace.tests.difficulty;
+
+import org.junit.Test;
+
+public class difficultyTest
+{
+
+    @Test
+    public void difficultySelectionTest()
+    {
+
+    }
+
+    @Test
+    public void obstacleIncrease()
+    {
+
+    }
+
+    @Test
+    public void powerupDecrease()
+    {
+
+    }
+
+
+}
