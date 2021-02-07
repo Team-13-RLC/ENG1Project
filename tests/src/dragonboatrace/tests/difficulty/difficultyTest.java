77182d4a103032eabdd4c5bdd0fc3297b8178594
NamLDuong <namlinhduong@gmail.com>
2021-02-07 19:56:14 +0000

    more on obstacle testing

--- tests/src/dragonboatrace/tests/difficulty/difficultyTest.java
+++ tests/src/dragonboatrace/tests/difficulty/difficultyTest.java
@@ -1,36 +1,58 @@
 package dragonboatrace.tests.difficulty;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.dragonboatrace.DragonBoatRace;
 import com.dragonboatrace.entities.Collidable;
-import com.dragonboatrace.screens.MainGameScreen;
-import com.dragonboatrace.tools.Lane;
 import com.dragonboatrace.entities.boats.BoatType;
+
+import com.dragonboatrace.tools.Race;
+import dragonboatrace.tests.GdxTestRunner;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
+@RunWith(GdxTestRunner.class)
 public class difficultyTest {
 
     DragonBoatRace game;
-    MainGameScreen main;
+    Race race1, race2, race3;
 
     @Before
     public void setUp() throws Exception {
         game = mock(DragonBoatRace.class);
-        MainGameScreen main = new MainGameScreen(game, BoatType.AGILE);
+        game.create();
+        race1 = new Race(10000, BoatType.AGILE, 1);
+        race2 = new Race(10000, BoatType.AGILE, 2);
+        race3 = new Race(10000, BoatType.AGILE, 3);
     }
 
     @Test
     public void obstacleIncrease() {
-        for (int i = 0; i < 600; i++) {
-            main.render((float) 0.16);
+        for (int i = 0; i < 100; i++) {
+            race1.update(0.16f, game);
         }
-        ArrayList<Collidable> colList = main.getRace().getPlayer().getLane().getCollidables();
-        System.out.println(colList.size());
+        ArrayList<Collidable> colList1 = race1.getPlayer().getLane().getCollidables();
+        int size1 = colList1.size();
+
+        for (int i = 0; i < 100; i++) {
+            race2.update(0.16f, game);
+        }
+        ArrayList<Collidable> colList2 = race2.getPlayer().getLane().getCollidables();
+        int size2 = colList2.size();
+
+        for (int i = 0; i < 100; i++) {
+            race3.update(0.16f, game);
+        }
+        ArrayList<Collidable> colList3 = race3.getPlayer().getLane().getCollidables();
+        int size3 = colList3.size();
+
+        assertTrue(size1 == size2);
+
     }
 
     @Test
