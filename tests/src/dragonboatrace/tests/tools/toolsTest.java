ecb18993d57586fa71203545c7daa3f5e4df56b0
NamLDuong <namlinhduong@gmail.com>
2021-02-08 21:28:25 +0000

    boundary detection test, trying to figure out the intersection tests

--- tests/src/dragonboatrace/tests/tools/toolsTest.java
+++ tests/src/dragonboatrace/tests/tools/toolsTest.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
 @RunWith(GdxTestRunner.class)
 public class toolsTest {
 
-    Hitbox boxA = new Hitbox(0, 2, 5, 1);
-    Hitbox boxB = new Hitbox(3, 2, 5, 1);
+    Hitbox boxA = new Hitbox(0, 0, 5, 1);
+    Hitbox boxB = new Hitbox(5, 0, 5, 1);
     Vector2 laneVector = new Vector2(0, 0);
     Lane laneTest = new Lane(laneVector, 10, 1);
 
@@ -30,7 +30,7 @@ public class toolsTest {
     public void boundaryDetectionTest() {
         assertEquals(false, laneTest.getHitbox().leaves(boxB));
         // moving the hitbox A outside of the lane's area.
-        boxB.move(50, 10);
+        boxB.move(-2, 0);
         assertEquals(true, laneTest.getHitbox().leaves(boxB));
     }
 
